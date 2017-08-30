a9f6086f80cb6a411f3cd021ecf1c73bf2cbf9cc
Devansh Patel <pateldd@goldmail.etsu.edu>
2017-08-30 03:42:44 -0700

    carousel for trips done

--- frontend/components/trip/trip_index.jsx
+++ frontend/components/trip/trip_index.jsx
@@ -22,14 +22,44 @@ class TripIndex extends React.Component {
 
   allTrips(){
     let theTrips = [];
+    let indicators = [];
     this.props.trips.forEach((trip, idx) => {
-      theTrips.push(
-        <div className="tripbox" key={idx}>
-          <TripIndexItem currentUser={this.props.currentUser} trip={trip} deleteTrip={this.props.deleteTrip}/>
-        </div>
-      )
+      if ( idx === 0 )
+      {
+        indicators.push(
+          <li key={idx} data-target="#myCarousel" data-slide-to={idx} className="active"></li>
+        )
+        theTrips.push(
+          <TripIndexItem key={idx} act={true} currentUser={this.props.currentUser} trip={trip} deleteTrip={this.props.deleteTrip}/>
+        )
+      }
+      else{
+        indicators.push(
+          <li key={idx} data-target="#myCarousel" data-slide-to={idx}></li>
+        )
+        theTrips.push(
+          <TripIndexItem key={idx} act={false} currentUser={this.props.currentUser} trip={trip} deleteTrip={this.props.deleteTrip}/>
+        )
+      }
     })
-    return theTrips;
+    return (
+      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          {indicators}
+        </ol>
+        <div className="carousel-inner">
+          {theTrips}
+        </div>
+        <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span className="glyphicon glyphicon-chevron-left"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="right carousel-control" href="#myCarousel" data-slide="next">
+          <span className="glyphicon glyphicon-chevron-right"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    )
   }
 
   render() {
@@ -38,7 +68,6 @@ class TripIndex extends React.Component {
     if (allTheTrips.length === 0) {
       return (
         <div className="tripOverView">
-          <span className="yourtravels">Your Travels</span>
           <div className="textfortrip">
             You need to travel more...
           </div>
@@ -48,15 +77,59 @@ class TripIndex extends React.Component {
     } else {
       return (
         <div className="tripOverView">
-          <span className="yourtravels">Your Travels</span>
-          <div className="trip-cards">
             {allTheTrips}
-          </div>
           <Link to="/" className="tosubmitdarkblue book-btn trip-button">Book another trip!</Link>
         </div>
       )
     }
   }
 }
+
+//
+// <div id="myCarousel" class="carousel slide" data-ride="carousel">
+//   <!-- Indicators -->
+//   <ol class="carousel-indicators">
+//     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+//     <li data-target="#myCarousel" data-slide-to="1"></li>
+//     <li data-target="#myCarousel" data-slide-to="2"></li>
+//   </ol>
+//
+//   <!-- Wrapper for slides -->
+//   <div class="carousel-inner">
+//     <div class="item active">
+//       <img src="la.jpg" alt="Chania">
+//       <div class="carousel-caption">
+//         <h3>Los Angeles</h3>
+//         <p>LA is always so much fun!</p>
+//       </div>
+//     </div>
+//
+//     <div class="item">
+//       <img src="chicago.jpg" alt="Chicago">
+//       <div class="carousel-caption">
+//         <h3>Chicago</h3>
+//         <p>Thank you, Chicago!</p>
+//       </div>
+//     </div>
+//
+//     <div class="item">
+//       <img src="ny.jpg" alt="New York">
+//       <div class="carousel-caption">
+//         <h3>New York</h3>
+//         <p>We love the Big Apple!</p>
+//       </div>
+//     </div>
+//   </div>
+//
+//   <!-- Left and right controls -->
+//   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+//     <span class="glyphicon glyphicon-chevron-left"></span>
+//     <span class="sr-only">Previous</span>
+//   </a>
+//   <a class="right carousel-control" href="#myCarousel" data-slide="next">
+//     <span class="glyphicon glyphicon-chevron-right"></span>
+//     <span class="sr-only">Next</span>
+//   </a>
+// </div>
 
 export default withRouter(TripIndex);
--- frontend/components/trip/trip_index_item.jsx
+++ frontend/components/trip/trip_index_item.jsx
@@ -23,45 +23,62 @@ class TripIndexItem extends React.Component {
 
   render() {
     const {trip} = this.props;
-
+    return (
+      <div className={this.props.act ? "item active" : "item"}>
+        <img src={trip.image_url} />
+        <div className="carousel-caption">
+          <Link to={`/homes/${trip.home.id}`} >
+            <h3 className="caro">{trip.home.title}</h3>
+          </Link>
+          <p className="caro">From {trip.start_date} to {trip.end_date}</p>
+          <p className="caro">{trip.home.address}</p>
+          <p className="caro">${trip.totalcost} was paid</p>
+          <p className="caroline" onClick={this.handleClick}>Click to Delete Trip</p>
+        </div>
+     </div>
+    )
     //the user has many trips and this join table association was made
     //the trip belongs to a home by home_id and this association was made. the home is just an object.
-    return (
-      <div className='trip-card'>
-        <div className='layer-for-trip'>
-          <div className='tripicturer'>
-            <Link to={`/homes/${trip.home.id}`} >
-              <img className='image-of-trip' src={trip.image_url}/>
-            </Link>
-          </div>
-          <div className='textfortrip'>
-
-            <div className='timefortrip'>
-              <div className='boxsched'>
-                <div className='the-box-address'>{trip.home.title}</div>
-
-                <div className="the-top-space">{trip.start_date} to {trip.end_date}</div>
-                <div className="the-top-space">{trip.home.address}</div>
-              </div>
-            </div>
-
-            <div className="divider-for-trip"/>
-              <div className="trip-make-action">
-
-              </div>
-            <div className='trip-make-action'>
-              <div className='occurrences-for-trip'>${trip.totalcost} was Paid</div>
-            </div>
-
-            <div className="divider-for-trip"/>
-            <div className="trip-make-action">
-              <div className='occurrences-for-trip cancel-trip' onClick={this.handleClick}>Remove / Cancel</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
   }
 }
 
 export default TripIndexItem;
+
+
+
+
+// return (
+//   <div className='trip-card'>
+//     <div className='layer-for-trip'>
+//       <div className='tripicturer'>
+//         <Link to={`/homes/${trip.home.id}`} >
+//           <img className='image-of-trip' src={trip.image_url}/>
+//         </Link>
+//       </div>
+//       <div className='textfortrip'>
+//
+//         <div className='timefortrip'>
+//           <div className='boxsched'>
+//             <div className='the-box-address'>{trip.home.title}</div>
+//
+//             <div className="the-top-space">{trip.start_date} to {trip.end_date}</div>
+//             <div className="the-top-space">{trip.home.address}</div>
+//           </div>
+//         </div>
+//
+//         <div className="divider-for-trip"/>
+//         <div className="trip-make-action">
+//
+//         </div>
+//         <div className='trip-make-action'>
+//           <div className='occurrences-for-trip'>${trip.totalcost} was Paid</div>
+//         </div>
+//
+//         <div className="divider-for-trip"/>
+//         <div className="trip-make-action">
+//           <div className='occurrences-for-trip cancel-trip' onClick={this.handleClick}>Remove / Cancel</div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// )
